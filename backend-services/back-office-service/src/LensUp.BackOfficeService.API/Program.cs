5dbfecf9181d7190fc42d0a5326efd8c130c1906
Kamil <dev.olszewski@gmail.com>
2024-03-15 07:46:20 +0100

    Feature: Add exception handler.

--- backend-services/back-office-service/src/LensUp.BackOfficeService.API/Program.cs
+++ backend-services/back-office-service/src/LensUp.BackOfficeService.API/Program.cs
@@ -22,7 +22,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseExceptionHandler();
 
 app.MapControllers();
 
---
+++ backend-services/back-office-service/src/LensUp.BackOfficeService.Application/GlobalExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LensUp.BackOfficeService.Application;
+
+public sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var details = new ProblemDetails()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Server Error",
+        };
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        await httpContext.Response.WriteAsJsonAsync(details, cancellationToken);
+
+        return true;
+    }
+}
