5f5ed53e7aef042138b3b5208d54267af9c29c86
Kamil <dev.olszewski@gmail.com>
2024-03-18 07:56:58 +0100

    Feature: Add AzureBlobStorage package to Common.

---
+++ backend-services/common/src/LensUp.Common.AzureBlobStorage/Models/PhotoToUpload.cs
@@ -0,0 +1,23 @@
+﻿namespace LensUp.Common.AzureBlobStorage.Models;
+
+public sealed class PhotoToUpload
+{
+    public string Id { get; init; }
+
+    public string FileName { get; init; }
+
+    public Stream Content { get; init; }
+
+    private PhotoToUpload(string id, string fileName, Stream content)
+    {
+        this.Id = id;
+        this.FileName = fileName;
+        this.Content = content;
+    }
+
+    public static PhotoToUpload Create(string id, string fileName, Stream content)
+    {
+        // TODO: validation
+        return new PhotoToUpload(id, fileName, content);
+    }
+}
---
+++ backend-services/common/src/LensUp.Common.AzureBlobStorage/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LensUp.Common.AzureBlobStorage.BlobStorage;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LensUp.Common.AzureBlobStorage;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAzureBlobStorage(this IServiceCollection services, string connectionString)
+    {
+        services.AddAzureClients(builder =>
+        {
+            builder.AddBlobServiceClient(connectionString);
+        });
+
+        services.AddScoped<IBlobStorageService, BlobStorageService>();
+
+        return services;
+    }
+}
