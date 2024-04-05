c78ccfc2e5b7dc805456aa94e2491fbaf7f943e2
Kamil <dev.olszewski@gmail.com>
2024-04-05 16:19:25 +0200

    Feature: Add Tailwind to UI monorepo projects.

--- ui-applications/packages/photo-collector-ui/src/App.tsx
+++ ui-applications/packages/photo-collector-ui/src/App.tsx
@@ -4,6 +4,9 @@ import { DummyComponent } from '@lens-up/shared-components'
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world tailwind styles!
+      </h1>
       <div>Photo Collector UI</div>
       <DummyComponent></DummyComponent>
     </>
