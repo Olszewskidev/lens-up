8207e018f2d6434d7eeeb9d8152236c135561642
Jan Safronov <jansafronov@gmail.com>
2024-05-18 20:49:42 +0300

    Start gallery unit tests

---
+++ ui-applications/packages/gallery-ui/tests/services/Gallery.test.tsx
@@ -0,0 +1,37 @@
+import {fireEvent, render, screen} from '@testing-library/react'
+import {expect, it} from '@jest/globals';
+import { LoginPage } from '../../src/pages/Login/LoginPage';
+import { galleryApi, useLoginToGalleryMutation } from '../../src/services/GalleryApi';
+import { LoginToGalleryPayload } from '../../src/types/GalleryApiTypes';
+
+it('Login must be accepted for the gallery', () => {
+    const {getByText, asFragment} = render(
+        <LoginPage />,
+    );
+
+    let payload = { enterCode: "BCD" } as LoginToGalleryPayload;
+
+    galleryApi.endpoints.loginToGallery.initiate(payload);
+
+    const firstRender = asFragment();
+
+    fireEvent.change(getByText("Login to your gallery"));
+    
+    expect(firstRender).toMatchSnapshot(asFragment());
+  });
+
+it('Gallery should show photos', () => {
+    const {getByText, asFragment} = render(
+        <LoginPage />,
+    );
+
+    let payload = { enterCode: "BCD" } as LoginToGalleryPayload;
+
+    galleryApi.endpoints.getGalleryPhotos.initiate("test");
+
+    const firstRender = asFragment();
+
+    fireEvent.change(getByText("Login to your gallery"));
+    
+    expect(firstRender).toMatchSnapshot(asFragment());
+  });
