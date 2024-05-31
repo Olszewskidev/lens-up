d2d21e7cb9e916c31b3980aebafa151d3d2d3d83
Kamil <dev.olszewski@gmail.com>
2024-05-31 20:55:22 +0200

    Feature: Carousel animation.

--- ui-applications/packages/gallery-ui/src/pages/Home/components/PhotoGallery.tsx
+++ ui-applications/packages/gallery-ui/src/pages/Home/components/PhotoGallery.tsx
@@ -2,15 +2,15 @@ import { useEffect, useMemo, useState } from "react";
 import { PhotoItem } from "../../../types/GalleryApiTypes"
 import { motion } from "framer-motion";
 import { getPhotoVariants } from "../utils/photoGalleryHelper";
-import { Position } from "../utils/constants";
+import { Position, pageSize, positionIndexesArray } from "../utils/constants";
 
 interface IPhotoGalleryProps {
     photoItems: PhotoItem[]
 }
 
 const PhotoGallery = ({ photoItems }: IPhotoGalleryProps) => {
+    const [positionIndexes, setPositionIndexes] = useState(positionIndexesArray);
 
-    const [positionIndexes, setPositionIndexes] = useState(photoItems.map((_, index) => index));
     const handleNext = () => {
         setPositionIndexes((prevIndexes) => {
             const updatedIndexes = prevIndexes.map(
@@ -21,13 +21,15 @@ const PhotoGallery = ({ photoItems }: IPhotoGalleryProps) => {
     };
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            handleNext();
-        }, 5000);
+        if (photoItems.length === pageSize) {
+            const interval = setInterval(() => {
+                handleNext();
+            }, 5000);
 
-        return () => {
-            clearInterval(interval);
-        };
+            return () => {
+                clearInterval(interval);
+            };
+        }
     }, [photoItems]);
 
 
@@ -47,10 +49,11 @@ const PhotoGallery = ({ photoItems }: IPhotoGalleryProps) => {
                     initial={Position.Center}
                     animate={positions[positionIndexes[index]]}
                     variants={variants}
-                    transition={{ duration: 0.75 }}
+                    transition={{ duration: 1 }}
                     style={{ width: "40%", position: "absolute" }}
                 />
-            ))}
+            )
+            )}
         </div>
     )
 }
