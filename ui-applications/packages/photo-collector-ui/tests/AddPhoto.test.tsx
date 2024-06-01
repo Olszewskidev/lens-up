1ca5002a6749984788a21a1a974089ccd31522b0
Jan Safronov <jansafronov@gmail.com>
2024-06-01 21:05:12 +0300

    Fix photo addition form tests

--- ui-applications/packages/photo-collector-ui/tests/AddPhoto.test.tsx
+++ ui-applications/packages/photo-collector-ui/tests/AddPhoto.test.tsx
@@ -9,11 +9,11 @@ import { BrowserRouter, Navigate, Route, RouterProvider, Routes } from "react-ro
 import userEvent from '@testing-library/user-event'
 import App from "../src/App";
 import AppRouter from "../src/pages/AppRouter";
-import { SuccessCardComponent } from "@lens-up/shared-components";
+import { ErrorCardComponent, SuccessCardComponent } from "@lens-up/shared-components";
 
 let file = new File(["happy"], "happy.png", { type: "image/png" });
 
-const handlers = [
+let handlers = [
     http.post(`${import.meta.env.VITE_PHOTO_COLLECTOR_SERVICE_URL}/upload-photo/0`, async () => {
         return new Response(null, {
             status: 200,
@@ -23,6 +23,10 @@ const handlers = [
           })
     })
 ]
+
+const successCardRender = render(
+    <SuccessCardComponent title="Congratulations!" text="You just joined to the party." />
+).asFragment();
 
 /**
 * @vitest-environment jsdom
@@ -72,8 +76,70 @@ describe("Add photo to gallery page tests", () => {
 
         fireEvent.submit(screen.getByRole('button'));
 
+        let successRender = asFragment();
+
         await waitFor(() => {
-            expect(asFragment()).toMatchSnapshot();
-        });
+            successRender = asFragment();
+            expect(successRender).toEqual(successCardRender);
+        }).then(() => expect(successRender).toMatchSnapshot());
+    })
+});
+
+const errorCardRender = render(
+    <ErrorCardComponent />
+).asFragment();
+
+describe("Add photo to gallery page tests unsuccessfully", () => {
+
+    handlers = [
+        http.post(`${import.meta.env.VITE_PHOTO_COLLECTOR_SERVICE_URL}/upload-photo/0`, async () => {
+            return new Response(null, {
+                status: 400,
+                headers: {
+                  Allow: 'GET,HEAD,POST',
+                },
+              })
+        })
+    ]
+
+    const server = setupServer(...handlers);
+
+    beforeAll(() => {
+        server.listen()
+    })
+    afterEach(cleanup);
+    afterAll(() => {
+        server.close()
+    })
+
+    test("Photo must upload and render error card", async () => {
+        URL.createObjectURL = vi.fn();
+        const { container, baseElement, asFragment } = render(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/upload-photo/:enterCode" element={<AddPhotoToGalleryPage />} />
+                    </Routes>
+                    <Navigate to="/upload-photo/0" replace={true} />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        const element = container.querySelector("#dropzone-file") as HTMLElement;
+
+        fireEvent.change(element, { target: { files: [file] } });
+
+        fireEvent.change(screen.getByPlaceholderText("Your name"), { target: { value: "author" } });
+
+        fireEvent.change(screen.getByPlaceholderText("..."), { target: { value: "happy" } });
+
+        fireEvent.submit(screen.getByRole('button'));
+
+        let errorRender = asFragment();
+
+        await waitFor(() => {
+            errorRender = asFragment();
+            expect(errorRender).toEqual(errorCardRender);
+        }).then(() => expect(errorRender).toMatchSnapshot());
     })
 });
