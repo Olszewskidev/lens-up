1075c4ac150005d30b00645662ff2368c5a09460
Kamil <dev.olszewski@gmail.com>
2024-06-11 20:59:22 +0200

    Feature: Backend-services docs and remove redundant code.

--- ui-applications/packages/gallery-ui/src/pages/Home/utils/emojiHelper.ts
+++
@@ -1,6 +0,0 @@
-import { emoijs } from "./constants";
-
-export const getRandomEmoji = () => {
-    const randomIndex = Math.floor(Math.random() * emoijs.length);
-    return emoijs[randomIndex];
-}
