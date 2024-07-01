812eb11e2e962b235c06bc1409a3ba8aaee03b21
Kamil <dev.olszewski@gmail.com>
2024-07-01 20:21:22 +0200

    Add Contracts dll to service.

---
+++ backend-services/back-office-service/src/LensUp.BackOfficeService.Contracts/Events/GalleryActivatedEvent.cs
@@ -0,0 +1,29 @@
+﻿using LensUp.Common.Types.Events;
+
+namespace LensUp.BackOfficeService.Contracts.Events;
+
+public sealed class GalleryActivatedEvent : EventMessage<GalleryActivatedEventPayload>
+{
+    public GalleryActivatedEvent(GalleryActivatedEventPayload payload) : base(payload)
+    {
+    }
+}
+
+public sealed class GalleryActivatedEventPayload
+{
+    public GalleryActivatedEventPayload(string galleryId, DateTimeOffset endDate, int enterCode, string qrCodeUrl)
+    {
+        this.GalleryId = galleryId;
+        this.EndDate = endDate;
+        this.EnterCode = enterCode;
+        this.QRCodeUrl = qrCodeUrl;
+    }
+
+    public string GalleryId { get; init; }
+
+    public DateTimeOffset EndDate { get; init; }
+
+    public int EnterCode { get; init; }
+
+    public string QRCodeUrl { get; init; }
+}
