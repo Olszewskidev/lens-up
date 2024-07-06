d61be33cdc81d06dfc0b77ee0bcb9727bf0d0eb8
Kamil <dev.olszewski@gmail.com>
2024-07-06 20:47:25 +0200

    Fix: Event processor HttpClient.

--- backend-services/webhooks-trigger-simulator/Subscribers.cs
+++ backend-services/webhooks-trigger-simulator/Subscribers.cs
@@ -12,6 +12,6 @@ internal static class Subscribers
 
     public static readonly Dictionary<string, Uri[]> GalleryQueueWebhooks = new Dictionary<string, Uri[]>()
     {
-         { nameof(GalleryActivatedEvent), [] }
+         { nameof(GalleryActivatedEvent), [new Uri($"http://localhost:8082/Webhook/{nameof(GalleryActivatedEvent)}")] }
     };
 }
