e2728cbc359463f3d827eac43345144c8a174f69
Kamil <dev.olszewski@gmail.com>
2024-07-18 19:01:45 +0200

    Feature: EF Core init.

---
+++ backend-services/back-office-service/src/LensUp.BackOfficeService.Infrastructure/Database/BackOfficeApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LensUp.BackOfficeService.Infrastructure.Database;
+
+internal sealed class BackOfficeApplicationDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
