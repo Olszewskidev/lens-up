bc26276bba76d362533a76c78c4c6efa59809374
Kamil <dev.olszewski@gmail.com>
2024-08-13 20:01:43 +0200

    Feature: Add EF entities representations.

---
+++ backend-services/gallery-service/src/LensUp.GalleryService.Domain/Entities/ActiveGalleryEFEntity.cs
@@ -0,0 +1,28 @@
+﻿using LensUp.Common.Types.Database;
+
+namespace LensUp.GalleryService.Domain.Entities;
+
+public sealed class ActiveGalleryEFEntity : IBaseEntity
+{
+    private ActiveGalleryEFEntity(Guid id, int enterCode, DateTimeOffset startDate, DateTimeOffset endDate, string qRCodeUrl)
+    {
+        this.Id = id;
+        this.EnterCode = enterCode;
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+        this.QRCodeUrl = qRCodeUrl;
+        this.CreatedAt = DateTimeOffset.UtcNow;
+    }
+
+    public Guid Id { get; init; }
+    public int EnterCode { get; init; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset StartDate { get; init; }
+    public DateTimeOffset EndDate { get; init; }
+    public string QRCodeUrl { get; init; }
+
+    public static ActiveGalleryEFEntity Create(Guid id, int enterCode, DateTimeOffset startDate, DateTimeOffset endDate, string qRCodeUrl)
+    {
+        return new ActiveGalleryEFEntity(id, enterCode, startDate, endDate, qRCodeUrl);
+    }
+}
---
+++ backend-services/photo-collector-service/src/LensUp.PhotoCollectorService.API/DataAccess/ActiveGallery/ActiveGalleryEFEntity.cs
@@ -0,0 +1,26 @@
+﻿using LensUp.Common.Types.Database;
+
+namespace LensUp.PhotoCollectorService.API.DataAccess.ActiveGallery;
+
+public sealed class ActiveGalleryEFEntity : IBaseEntity
+{
+    private ActiveGalleryEFEntity(Guid id, int enterCode, DateTimeOffset startDate, DateTimeOffset endDate)
+    {
+        this.Id = id;
+        this.EnterCode = enterCode;
+        this.CreatedAt = DateTimeOffset.UtcNow;
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+    }
+
+    public Guid Id { get; init; }
+    public int EnterCode { get; init; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset StartDate { get; init; }
+    public DateTimeOffset EndDate { get; init; }
+
+    public static ActiveGalleryEFEntity Create(Guid id, int enterCode, DateTimeOffset startDate, DateTimeOffset endDate)
+    {
+        return new ActiveGalleryEFEntity(id, enterCode, startDate, endDate);
+    }
+}
